5f496444aa8de757a39ba5c9c5d637bbcd99d484
Apoorv Agrawal <apoorv03@gmail.com>
2012-12-02 19:26:02 +0800

    added google table to shop PC

--- storeadmin/inventory_list.php
+++ storeadmin/inventory_list.php
@@ -110,6 +110,79 @@ if ($productCount > 0) {
 <title>Inventory List</title>
  <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <!--<link rel="stylesheet" href="../style/style.css" type="text/css" media="screen" />-->
+
+<!--
+<link rel="stylesheet" href="../style/style.css" type="text/css" media="screen" />
+--><!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+    
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1', {'packages':['table']});
+      
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawTable);
+
+
+      // Callback that creates and populates a data table, 
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawTable() {
+
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+        data.addColumn('number', 'Barcode');
+        data.addColumn('string', 'Name');
+        data.addColumn('number', 'Price');
+        data.addColumn('string', 'Category');
+        data.addColumn('string', 'Manufacturer');
+        data.addColumn('number', 'quantity');
+        data.addColumn('string', 'Edit/Delete');
+      data.addRows([
+        <?php
+
+          $product_list = '';
+          $sql = mysql_query("SELECT * FROM Inventory ");
+          $productCount = mysql_num_rows($sql); // count the output amount
+          if ($productCount > 0) {
+            while($row = mysql_fetch_array($sql)){ 
+                 $id = $row["Barcode"];
+                 $product_name = $row["Product_Name"];
+                 $replace[] = ",";
+                 $product_name2 = str_replace("'" , "" , $product_name);
+                 $category = $row["Category"];
+                 $Manufacturer = $row["Manufacturer"];
+                 $price = $row["Cost_Price"];
+                 $current = $row["Current_Stock"];
+
+                 $product_list .= '[' . 
+                 $id . ',' . 
+                 '\''  . $product_name2 . '\'' . ',' . 
+                 $price . ',' . 
+                 '\'' . $category . '\'' . ',' . 
+                 '\'' . $Manufacturer . '\'' . ',' . 
+                 $current . ',' .
+                 "\"<a href='inventory_edit.php?pid=$id'>edit</a>  <a href='inventory_list.php?deleteid=$id'>delete</a>\"
+                 " . '],'; 
+              }
+          }
+          echo  $product_list ;
+        ?>
+      ]);
+
+      // Instantiate and draw our table, passing in some options.
+      var table = new google.visualization.Table(document.getElementById('barformat_div'));
+      var formatter = new google.visualization.ArrowFormat({width: 30, base:2000});
+      formatter.format(data, 5); // Apply formatter to sixth column
+  
+  table.draw(data, {allowHtml: true, showRowNumber: false});
+
+       // set the width of the column with the title "Name" to 100px
+     var title = "Name";
+     var width = "200px";
+     $('.google-visualization-table-th:contains(' + title + ')').css('width', width);
+    }
+    </script>
 </head>
 
 <body>
@@ -121,7 +194,7 @@ if ($productCount > 0) {
     <div align="right" style="margin-right:32px;"><a href="inventory_list.php#inventoryForm">+ Add New Inventory Item</a></div>
 <div align="left" style="margin-left:24px;">
       <h2>Inventory list</h2>
-      <?php echo $product_list; ?>
+            <div id='barformat_div'></div>
     </div>
     <hr />
     <a name="inventoryForm" id="inventoryForm"></a>
