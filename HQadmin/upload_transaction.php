06daeaa16d2e0fdc9dbbd65318d8b454b28d4f09
Apoorv Agrawal <apoorv03@gmail.com>
2012-12-04 17:20:00 +0800

    saurabh test revert

--- HQadmin/upload_transaction.php
+++ HQadmin/upload_transaction.php
@@ -1,26 +1,69 @@
 <?php
-echo "in UploadTest.php file";
-//set where you want to store files
-//in this example we keep file in folder upload 
-//$HTTP_POST_FILES['ufile']['name']; = upload file name
-//for example upload file name cartoon.gif . $path will be upload/cartoon.gif
+include "../storescripts/connect_to_mysql_HQ.php"; 
+mysql_query($sql);
+mysql_query($sql2);
+mysql_query($sql3);
 
-$url = "http://ec2-23-20-78-149.compute-1.amazonaws.com/uploads/";
- 
-$path= "/var/www/uploads/".$_FILES['file']['name'];
-if($file != none)
-{
-	if(copy($_FILES['file']['tmp_name'], $path))
-	{
-		echo "Successful<BR/>";
-		 
-		echo "File Name :".$HTTP_POST_FILES['file']['name']."<BR/>"; 
-		echo "File Size :".$HTTP_POST_FILES['file']['size']."<BR/>"; 
-		echo "File Type :".$HTTP_POST_FILES['file']['type']."<BR/>";
+$filename= mysql_real_escape_string($_FILES["file"]["tmp_name"]);
+
+$lines = file($filename); // slurp file and split into an array by lines
+$counter = 0;
+foreach($lines as $line) {
+	if ($counter<1){
+		$shopID = $line;
 	}
-	else
-	{
-		echo "Error";
-	}
+	else{
+   	 	$parts = explode(':', $line); // decompose a line into individual sections
+		$Barcode = mysql_real_escape_string(trim($parts[0]));
+		$Cost_Price = mysql_real_escape_string(trim($parts[1]));
+		$Selling_Price = mysql_real_escape_string(trim($parts[2]));
+		$Quantity = mysql_real_escape_string(trim($parts[3]));
+		$Month = mysql_real_escape_string(trim($parts[4]));
+
+		/*$hash['name'] = "Vishesh";
+		$hash['barcode'] = 'JSHKJS';
+		$hash2['one']= $hash;
+		$hash2['two'] = "Vish21";
+		print_r($hash2['one']['name']);
+		foreach($hash2 as $key=>$value){
+			1st loop
+			key = one
+			$value['name'];
+			$value[]
+		}
+		$hash2[] = "Vishesh";*/
+
+		$revenue += $Selling_Price*$Quantity;
+		$cost += $Cost_Price*$Quantity;
+		$unitsSold += $Quantity;
+		
+		$sql2 = "INSERT INTO Transactions (shopID, Barcode, Cost, Price, Quantity, Month) 
+		VALUES ('$shopID', '$Barcode', '$Cost_Price', '$Selling_Price', '$Quantity', '$Month')";
+		
+		mysql_query($sql2) or die(mysql_error());
+		
+		$sql3 = mysql_query("SELECT Inventory.Current_Stock FROM Inventory where Barcode = '$Barcode'");
+		echo $sql3;
+		$curUnits = 0;
+		while($row=mysql_fetch_array($sql3)){
+			$curUnits = $row[0];
+		}
+		$curUnits = $curUnits - $Quantity;
+		$sql4 = mysql_query("UPDATE Inventory SET Current_Stock='$curUnits' WHERE Barcode='$Barcode'");
+	}	
+	$counter++;
 }
+$profits = $revenue - $cost;
+
+$sql = "INSERT INTO shopPerformance (month, shopID, revenue, profits, cost, unitsSold) 
+VALUES ('$Month', '$shopID', '$revenue', '$profits', '$cost', '$unitsSold')";
+
+if (mysql_num_rows(mysql_query("SELECT * from shopPerformance where shopID = '$shopID' and where month = '$Month' ")) > 0) {
+	$sql = "UPDATE shopPerformance SET  revenue = '$revenue', profits = '$profits', cost = '$cost', unitsSold = '$unitsSold' 
+	where shopID = '$shopID' and month = '$Month' ";
+}
+mysql_query($sql) or die(mysql_error());
+
+echo '<META HTTP-EQUIV="Refresh" Content="0; URL=transaction.php">';    
+    exit;    
 ?>
--- storescripts/connect_to_mysql_HQ.php
+++ storescripts/connect_to_mysql_HQ.php
@@ -1,23 +1,69 @@
-<?php  
-// This file is www.developphp.com curriculum material
-// Written by Adam Khoury January 01, 2011
-// http://www.youtube.com/view_play_list?p=442E340A42191003
-/*  
-1: "die()" will exit the script and show an error statement if something goes wrong with the "connect" or "select" functions. 
-2: A "mysql_connect()" error usually means your username/password are wrong  
-3: A "mysql_select_db()" error usually means the database does not exist. 
-*/ 
-// Place db host name. Sometimes "localhost" but  
-// sometimes looks like this: >>      ???mysql??.someserver.net 
-$db_host = "localhost"; 
-// Place the username for the MySQL database here 
-$db_username = "root";  
-// Place the password for the MySQL database here 
-$db_pass = "password";  
-// Place the name for the MySQL database here 
-$db_name = "HQ"; 
+<?php
+include "../storescripts/connect_to_mysql_HQ.php"; 
+mysql_query($sql);
+mysql_query($sql2);
+mysql_query($sql3);
 
-// Run the actual connection here  
-mysql_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
-mysql_select_db("$db_name") or die ("no database");              
+$filename= mysql_real_escape_string($_FILES["file"]["tmp_name"]);
+
+$lines = file($filename); // slurp file and split into an array by lines
+$counter = 0;
+foreach($lines as $line) {
+	if ($counter<1){
+		$shopID = $line;
+	}
+	else{
+   	 	$parts = explode(':', $line); // decompose a line into individual sections
+		$Barcode = mysql_real_escape_string(trim($parts[0]));
+		$Cost_Price = mysql_real_escape_string(trim($parts[1]));
+		$Selling_Price = mysql_real_escape_string(trim($parts[2]));
+		$Quantity = mysql_real_escape_string(trim($parts[3]));
+		$Month = mysql_real_escape_string(trim($parts[4]));
+
+		/*$hash['name'] = "Vishesh";
+		$hash['barcode'] = 'JSHKJS';
+		$hash2['one']= $hash;
+		$hash2['two'] = "Vish21";
+		print_r($hash2['one']['name']);
+		foreach($hash2 as $key=>$value){
+			1st loop
+			key = one
+			$value['name'];
+			$value[]
+		}
+		$hash2[] = "Vishesh";*/
+
+		$revenue += $Selling_Price*$Quantity;
+		$cost += $Cost_Price*$Quantity;
+		$unitsSold += $Quantity;
+		
+		$sql2 = "INSERT INTO Transactions (shopID, Barcode, Cost, Price, Quantity, Month) 
+		VALUES ('$shopID', '$Barcode', '$Cost_Price', '$Selling_Price', '$Quantity', '$Month')";
+		
+		mysql_query($sql2) or die(mysql_error());
+		
+		$sql3 = mysql_query("SELECT Inventory.Current_Stock FROM Inventory where Barcode = '$Barcode'");
+		echo $sql3;
+		$curUnits = 0;
+		while($row=mysql_fetch_array($sql3)){
+			$curUnits = $row[0];
+		}
+		$curUnits = $curUnits - $Quantity;
+		$sql4 = mysql_query("UPDATE Inventory SET Current_Stock='$curUnits' WHERE Barcode='$Barcode'");
+	}	
+	$counter++;
+}
+$profits = $revenue - $cost;
+
+$sql = "INSERT INTO shopPerformance (month, shopID, revenue, profits, cost, unitsSold) 
+VALUES ('$Month', '$shopID', '$revenue', '$profits', '$cost', '$unitsSold')";
+
+if (mysql_num_rows(mysql_query("SELECT * from shopPerformance where shopID = '$shopID' and where month = '$Month' ")) > 0) {
+	$sql = "UPDATE shopPerformance SET  revenue = '$revenue', profits = '$profits', cost = '$cost', unitsSold = '$unitsSold' 
+	where shopID = '$shopID' and month = '$Month' ";
+}
+mysql_query($sql) or die(mysql_error());
+
+echo '<META HTTP-EQUIV="Refresh" Content="0; URL=transaction.php">';    
+    exit;    
 ?>
